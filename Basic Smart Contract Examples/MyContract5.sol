94f89b28201b05e0ba8a288920db3557193596d1
Hui Gong <40460281+gonghui945@users.noreply.github.com>
2021-02-14 15:07:45 +0000

    Add files via upload

---
+++ Basic Smart Contract Examples/MyContract5.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.1;
+
+contract MyContract{
+    string public constant stringvalue = "MyString";
+    bool public myBool = true;
+    int public myInt = -1;
+    uint public myUint = 1;
+    uint8 public myUnit8 = 8;
+    uint256 public myUnit256 = 9999;
+}
