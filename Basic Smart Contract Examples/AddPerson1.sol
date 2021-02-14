3f6d4c94fcb8e2d321bf726fb5ae47fcc52a03d6
Hui Gong <40460281+gonghui945@users.noreply.github.com>
2021-02-14 15:09:35 +0000

    Add files via upload

---
+++ Basic Smart Contract Examples/AddPerson1.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.1;
+
+contract AddPerson{
+    
+    Person[] public people;
+    
+    struct Person{
+        string _firstName;
+        string _lastName;
+    }
+    
+    function addPerson(string memory _firstName, string memory _lastName) public {
+        people.push(Person(_firstName,_lastName));
+    }
+}
